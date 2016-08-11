91165bfc31e7de7b4b27b8c25dfd58a750ad8baa
Terry Hayes <terrychayes@gmail.com>
2016-08-10 22:05:55 -0400

    Added self.create method to Udacidata class. Passing first two tests (re: create method).

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,16 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+  def self.create(opts={})
+  	@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+  	CSV.open(@data_path, "a+") do |csv|
+  		csv << [@id, opts[:brand], opts[:name], opts[:price]]
+  		#Not sure if @id is working properly yet.
+  	end
+  	Product.new(opts={}) #Perhaps this should be called at the beginning of method, then returned here.
+  end
+  
+  def self.all
+  	return [] #Silly initial pass.
+  end
 end
