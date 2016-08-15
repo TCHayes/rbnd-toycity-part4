8c58cdb829bf4a3f5cc2edaf6f812b2d789fd33d
Terry Hayes <terrychayes@gmail.com>
2016-08-15 16:01:45 -0400

    Adds Faker gem data to seeds.rb. Udacidata create method updated, but still needs to be fixed for modularity. Adds first and last methods; both appear to be working properly.

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,16 +3,37 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  def self.create(opts={})
-  	@data_path = File.dirname(__FILE__) + "/../data/data.csv"
-  	CSV.open(@data_path, "a+") do |csv|
-  		csv << [@id, opts[:brand], opts[:name], opts[:price]]
-  		#Not sure if @id is working properly yet.
-  	end
-  	Product.new(opts={}) #Perhaps this should be called at the beginning of method, then returned here.
+
+@@products = []
+@@data_path = "../data/data.csv"
+
+  def self.create(opts={}) #This needs to be rewritten with modularity in mind. (Customer and Transaction classes will also inherit)
+    new_product = Product.new(brand: opts[:brand], name: opts[:name], price: opts[:price])
+    @@products << new_product
+    #Still need to check if id already in database before adding...
+    CSV.open(@@data_path, "a+") do |csv|
+      csv << [new_product.id, opts[:brand], opts[:name], opts[:price]]
+    end
+    new_product    
   end
   
   def self.all
-  	return [] #Silly initial pass.
+    @@products
+  end
+
+  def self.first(amount = 1)
+    if amount == 1
+      @@products.first
+    else
+      @@products.first(amount)
+    end
+  end
+
+  def self.last(amount = 1)
+    if amount == 1
+      @@products.last
+    else
+      @@products.last(amount)
+    end
   end
 end
