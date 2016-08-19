a01b69d19d6e77eb14be0a856fd8f539cb1b40c7
Terry Hayes <terrychayes@gmail.com>
2016-08-18 21:38:39 -0400

    Minor cleanup edits to udacidata.rb

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -8,21 +8,22 @@ class Udacidata
 
   def self.create(**opts)
     new_item = self.new(**opts)
-    #Still need to check if id already in database before adding...
+    # Still need to check if id already in database before adding...
     CSV.open(@@data_path, "a+") do |csv|
-      #Might need to alter line below for use w different Classes (beyond Products)
+      # Ideally would alter this for use w/ Transaction and Customer classes:
       csv << [new_item.id, new_item.brand, new_item.name, new_item.price]
     end
     new_item
   end
   
   def self.all
-    @products = [] #Question: Should this array have the @ sign in front?
+    products = []
     data_array = CSV.read(@@data_path)
     data_array.drop(1).each do |row|
-      @products << self.create(id: row[0], brand: row[1], name: row[2], price: row[3])
+      # Another modularity conflict:
+      products << self.create(id: row[0], brand: row[1], name: row[2], price: row[3])
     end
-    @products
+    products
   end
 
   def self.first(amount = 1)
